ac7a6a182a81238f08e0f21dbade3d77f5c37145
camilletti <camilletti92@gmail.com>
2015-12-29 16:05:07 +0100

    ooo

--- admin_page.php
+++ admin_page.php
@@ -4,8 +4,8 @@ UserCake Version: 2.0.2
 http://usercake.com
 */
 
-require_once("models/config.php");
-if (!securePage($_SERVER['PHP_SELF'])){die();}
+//require_once("models/config.php");
+//if (!securePage($_SERVER['PHP_SELF'])){die();}
 $pageId = $_GET['id'];
 
 //Check if selected pages exist
